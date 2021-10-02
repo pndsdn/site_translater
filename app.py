f1cac46f8b3c736542f77d80110f1ba2e7ecb479
pndsdn <saa020403@gmail.com>
2021-10-02 19:44:35 +0300

    from en to ru

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+import requests
+import os
+import uuid
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+# сделать выбор языка
+# асинхронный JS запрос для обновления результата
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def dict_page():
+    return render_template('dict.html')
+
+@app.route('/translate', methods=['POST'])
+def index_post():
+    original_text = request.form['word']
+    target_language = "ru"
+    print(original_text)
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+
+    path = '/translate?api-version=3.0&'
+
+    target_language_parameter = 'from=en&to=' + target_language
+
+    constructed_url = endpoint + path + target_language_parameter
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+
+    body = [{'text': original_text}]
+
+    translator_request = requests.post(
+        constructed_url, headers=headers, json=body)
+
+    translator_response = translator_request.json()
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    return render_template(
+        'result.html',
+        word=translated_text
+    )
+
+if __name__ == '__main__':
+    app.run()
