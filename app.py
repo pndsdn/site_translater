eb81924db94e3abc62035b45b95d39e9bd2b3203
pndsdn <saa020403@gmail.com>
2021-10-08 15:03:26 +0300

    not asynchronous response

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import uuid
 import json
 from dotenv import load_dotenv
 load_dotenv()
-# url_for('static', filename='script.js')
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -14,10 +13,11 @@ def dict_page():
 
 @app.route('/translate', methods=['POST'])
 def index_post():
-
     original_text = request.form['word']
+    print(original_text)
     from_language = request.form['from']
     to_language = request.form['to']
+    print(to_language)
 
     key = os.environ['KEY']
     endpoint = os.environ['ENDPOINT']
@@ -25,7 +25,7 @@ def index_post():
 
     path = '/translate?api-version=3.0&'
 
-    target_language_parameter = 'from=' + from_language + '&to=' + to_language
+    target_language_parameter = 'from=en&to=' + to_language
 
     constructed_url = endpoint + path + target_language_parameter
 
@@ -38,13 +38,14 @@ def index_post():
 
     body = [{'text': original_text}]
 
-    translator_request = requests.post(
-        constructed_url, headers=headers, json=body)
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
 
     translator_response = translator_request.json()
     translated_text = translator_response[0]['translations'][0]['text']
 
-    return jsonify(translated_text)
+    #return jsonify({"translated_text": translated_text})
+
+    return render_template('result.html', word = translated_text)
 
 if __name__ == '__main__':
     app.run()
