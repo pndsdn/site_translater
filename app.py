156dd67dc3ea8abbc88b1a201d68166cbededa72
pndsdn <saa020403@gmail.com>
2021-10-09 09:53:10 +0300

    comments added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, url_for
+from flask import Flask, render_template, request, jsonify
 import requests
 import os
 import uuid
@@ -7,28 +7,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def dict_page():
     return render_template('dict.html')
 
+
 @app.route('/translate', methods=['POST'])
 def index_post():
-    original_text = request.form['word']
+    original_text = request.json['translate'].lower()  # получаем значение из запроса по ключу 'translate'
     print(original_text)
-    from_language = request.form['from']
-    to_language = request.form['to']
+    from_language = request.json['from']  # получаем значение из запроса по ключу 'from'
+    print(from_language)
+    to_language = request.json['to']  # получаем значение из запроса по ключу 'to'
     print(to_language)
 
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
-
+    # здесь создаётся ссылка для запроса
+    key = os.environ['KEY']                # значения key,
+    endpoint = os.environ['ENDPOINT']      # endpoint,
+    location = os.environ['LOCATION']      # location берутся из файла .env
     path = '/translate?api-version=3.0&'
+    target_language_parameter = 'from=' + from_language + '&to=' + to_language  # часть ссылки, которая отвечает за параметры перевода
+    constructed_url = endpoint + path + target_language_parameter  # готовая ссылка
 
-    target_language_parameter = 'from=en&to=' + to_language
-
-    constructed_url = endpoint + path + target_language_parameter
-
+    # заголовки обязательно передаются в запросе
     headers = {
         'Ocp-Apim-Subscription-Key': key,
         'Ocp-Apim-Subscription-Region': location,
@@ -36,16 +39,10 @@ def index_post():
         'X-ClientTraceId': str(uuid.uuid4())
     }
 
-    body = [{'text': original_text}]
+    body = [{'text': original_text}]  # тело запроса
+    translator_response = requests.post(constructed_url, headers=headers, json=body)  # запрос
+    return translator_response.json()  # возвращаем в виде JSON
 
-    translator_request = requests.post(constructed_url, headers=headers, json=body)
-
-    translator_response = translator_request.json()
-    translated_text = translator_response[0]['translations'][0]['text']
-
-    #return jsonify({"translated_text": translated_text})
-
-    return render_template('result.html', word = translated_text)
 
 if __name__ == '__main__':
     app.run()
